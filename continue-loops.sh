a883278fe9f3de1cba2e5776985224f2d73cc48d
aditya <adityanarra04@gmail.com>
2021-05-25 09:08:20 +0000

    third simple shell scripts

---
+++ continue-loops.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for each in 2 4 35 78 345 34
+
+do
+if [ $each -eq 78 ]
+then
+	continue
+fi
+ echo $each
+
+done
+
---
+++ loop-control-statements.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+i=1
+while true
+do
+  echo "$i"
+ if [ $i -eq 70 ]
+then
+break
+fi
+  i=$((i+1))
+
+
+done
